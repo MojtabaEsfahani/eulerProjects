8eefdfba3c6d4eea46347479e8ac2491b5abb7e7
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-20 16:49:59 +0200

    Update eulerProject2.py

--- eulerProject2.py
+++ eulerProject2.py
@@ -1,15 +1,23 @@
-# Generate Fibonacci sequence
+# Generate Fibonacci Sequence With Max-Value
+import os;
 def fElements(max):
-
     firstNumber = 1 ;
     secondNumber = 1 ;
     thirdNumber = 1 ;
-
+    sumElement = 0;
     while (thirdNumber<max):
         thirdNumber = firstNumber + secondNumber;
         firstNumber = secondNumber;
         secondNumber = thirdNumber;
-        
-    return secondNumber
+        if(firstNumber%2==0):
+            sumElement += firstNumber;                
+    return sumElement;
 
-print (fElements(40)) 
+os.system('cls')
+Max=input("\nchoice an Integer as Maximal-value : ");
+try:
+    Max=int(Max);
+    ans = fElements(Max);
+    print ("\n\n", ans, " is the sum of the even-valued terms.\n By considering in the Fibonacci sequence whose values do not exceed ", Max, ".\n\n")
+except:
+    print("Something Went Wrong !!!");
