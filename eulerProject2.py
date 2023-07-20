c3b9e1f383b745bfad6920e3d7ecfc65a12edba5
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-20 15:06:09 +0200

    Update eulerProject2.py

--- eulerProject2.py
+++ eulerProject2.py
@@ -1,15 +1,16 @@
 # Generate Fibonacci sequence
+def fElements(max):
 
-firstNumber = 1 ;
-secondNumber = 1 ;
-thirdNumber = 1 ;
+    firstNumber = 1 ;
+    secondNumber = 1 ;
+    thirdNumber = 1 ;
 
-
-while (thirdNumber<40):
+    while (thirdNumber<max):
     
         print(thirdNumber, " ,")
         thirdNumber = firstNumber + secondNumber;
         firstNumber = secondNumber;
         secondNumber = thirdNumber;
-        
+
+fElements(40)        
         
