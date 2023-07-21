68ce03e4425bf88bb13e1b789b831277a744df53
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-21 08:47:36 +0200

    Update eulerProject3.py

--- eulerProject3.py
+++ eulerProject3.py
@@ -1,29 +1,23 @@
 import os
-# Generate prime factors
+# Generate factors of a Number
 def factors(number, dividend):
     min = 2;
     for integer in range(min, number+1):
-        if(dividend%integer==0):
-        #     p=integer;
-        # while(dividend%integer==0):
-            
+        if(dividend%integer==0):            
             dividend = dividend/integer;
-            print (integer);
-            af(number, dividend)
-            
-def af(number, dividend):
-    if(int(dividend)==1):
-                return ;
-    factors(number, dividend);
-    
+            print (integer, " ,", sep="");
+            return integer, dividend;
+
 try:
-    os.system('cls')
+    Mal = 1;
+    os.system('cls');
     number=input("\nEnter an Integer Number : ");
     number=int(number);
     print ("The factors of", number, "are :\n\n");
     dividend = number;
-    Max = factors(number, dividend);
-    # print (Max)
+    while(Mal!=number):
+        Max, dividend = factors(number, dividend);
+        Mal *= Max
 except:
     print("Something Went Wrong !!!");
     
