ee831e4f3d64565622ac9903273e3808ee8bd26c
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-22 13:01:47 +0200

    Update eulerProject4.py

--- eulerProject4.py
+++ eulerProject4.py
@@ -1,15 +1,15 @@
 import os
 #generate palindromic numbers
 def pali_numbers (min, max):
-    com = 0;
+    max_pali = 0;
     return_packet =""
     for first_number in range (min, max):
         for second_number in range (min, max):
-            mul = first_number*second_number;
-            if (mul == reverse(mul) and mul > com):
-                com = mul;
-                
-    return ;
+            multiple = first_number*second_number;
+            if (multiple==reverse(multiple) and multiple>max_pali):
+                max_pali = multiple;
+                return_packet = "{}*{}*{}".format(str(first_number), str(second_number), str(max_pali));
+    return return_packet;
 
 
 def reverse (int_number):
@@ -19,7 +19,11 @@ def reverse (int_number):
 
 
 os.system ('cls');
-min = int(input("Enter a min range"));
-max = int(input("Enter a max range"));
-first_number, second_number, com = pali_numbers (min, max);
-print ("{} * {} = {}".format(first_number, second_number, com));
+try:
+    min = int(input("Enter a min range :"));
+    max = int(input("Enter a max range :"));
+    answer = pali_numbers (min, max).split('*');
+    print ("The maximum palindromic number between {} and {} is :".format(min, max))
+    print ("{} * {} = {}".format(answer[0], answer[1], answer[2]));
+except:
+    print ("something went wrong !!!")
