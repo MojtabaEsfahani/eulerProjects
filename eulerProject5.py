a91b216e4287da84abbf3883d14ba416cbf02075
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-25 13:11:08 +0200

    Update eulerProject5.py

--- eulerProject5.py
+++ eulerProject5.py
@@ -1,6 +1,22 @@
-def dividend (max):
-    number = 1
-    while(True):
-        number++
-        for x in range(1, max+1):
-            
+import os;
+
+def dividend ():
+    number=0;
+    bul=True;
+    while (bul or number==0):
+        RANGE = [2, 3, 4, 5, 7, 8, 9, 11, 13, 16, 17, 19];
+        (bul)=False;
+        number=number+20;
+        for i in RANGE:
+            if (number%i != 0):
+                (bul)=True;
+                break;
+    print (number);
+    
+try:
+    os.system('cls');
+    # min = int(input("Enter an integer as min :"));
+    # max = int(input("Enter an integer as max :"));
+    dividend();    
+except:
+    print ("something went wrong !!!")
