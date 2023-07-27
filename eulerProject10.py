82326b1123a12569f0a0bc506cc3fc6af48be0c2
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-27 14:51:34 +0200

    Update eulerProject10.py

--- eulerProject10.py
+++ eulerProject10.py
@@ -1,1 +1,15 @@
-# generate prime numbers
+import os            
+def primes(max):    
+    prime = []
+    primes = [True] * max
+    primes[0]=primes[1]=False
+    for i in range(2, max):
+        if primes[i] :
+            for j in range(i**2, max, i):
+                primes[j] = False;
+            prime.append(i)
+    print(prime)
+    
+os.system('cls')
+max = int(input("Enter a number as max :"))        
+primes(max)
