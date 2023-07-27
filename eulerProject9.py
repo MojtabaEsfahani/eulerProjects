91788326651bb27f212dd7f2196cdeca74791d9c
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-27 12:58:49 +0200

    Update eulerProject9.py

--- eulerProject9.py
+++ eulerProject9.py
@@ -1,11 +1,12 @@
+import os
 def generator():
     for a in range(1, 10000):
         for b in range(1, 10000):
             A = (1000-a-b)
             B = (a**2)+(b**2)
             if ((A**2) == B and A>a and A>b) :
-                print ("okay")
-                print (A, a, b, "\n")
-                
-                
-generator()
+                return (A*a*b)
+os.system ('cls')                              
+Ans = generator()
+print ("There exists exactly one Pythagorean triplet for which :\n a+b+c=1000")
+print ("the product abc is : {}".format(Ans))
