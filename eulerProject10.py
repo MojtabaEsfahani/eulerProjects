4de7f0a4d1c982a6307494da89f34b320ee03f6e
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-07-28 15:43:59 +0200

    Update eulerProject10.py

--- eulerProject10.py
+++ eulerProject10.py
@@ -1,15 +1,17 @@
 import os            
 def primes(max):    
-    prime = []
+    sum = 0
     primes = [True] * max
     primes[0]=primes[1]=False
     for i in range(2, max):
         if primes[i] :
             for j in range(i**2, max, i):
                 primes[j] = False;
-            prime.append(i)
-    print(prime)
+            sum += i
+    print(sum)
     
 os.system('cls')
 max = int(input("Enter a number as max :"))        
 primes(max)
+
+# 142913828922
