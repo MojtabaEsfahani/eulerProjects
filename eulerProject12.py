a9dfdda8987e65f7d236d952a8399219bf9ee8f4
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-08-26 22:25:10 +0200

    Update eulerProject12.py

--- eulerProject12.py
+++ eulerProject12.py
@@ -1,20 +1,37 @@
 import math
+
+
 def hash_divisors(number):
-    sqrt_number = int(math.sqrt(number));
-    t = 1;
+    sqrt_number = int(math.sqrt(number))
+    t = 0
     for i in range(1, sqrt_number+1):
         if number % i == 0:
-            t += 1;
+            t += 1
             if i != number/i:
-                t += 1;
-    if t>=500 :
-        print (number," is the Answer", sep="")
-        return False;
-    else :
-        return True;
+                t += 1
+    if t >= 500:
+        print(number, " is the Answer", sep="")
+        return True
+    else:
+        return False
 
 
-number =1;
-while (hash_divisors(number)):
-    number +=1;
-    
+def create_numbers(number):
+    number = (number*(number+1))/2
+    return int(number)
+
+
+# number = 1
+# for i in range(2, 10000000000):
+#     number +=i
+#     con = hash_divisors(number)
+#     if (con) :
+#         break
+
+
+counter = 1
+while(True):
+    Ans = create_numbers(counter)
+    counter += 1
+    if (hash_divisors(Ans)):
+        break
