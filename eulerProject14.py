cb9ca40899fdb63d382a70fd13fcfd1a9c3119f7
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-08-27 09:58:26 +0200

    Update eulerProject14.py

--- eulerProject14.py
+++ eulerProject14.py
@@ -4,16 +4,18 @@ def collatz_seq(start_number):
     seq = [start_number]
     while(True):
         if (seq[-1]==1) :
-            return len(seq)
+            return len(seq), seq[1]
         if (start_number%2 == 0):
             seq.append(start_number/2)
         else:
             seq.append((3*start_number)+1)
         start_number = seq[-1]
-    
-max = 0    
+
+max = 0
+st_num = 1
 for i in range(1, 14):
-    chain_len = collatz_seq(i)
+    chain_len, start_number = collatz_seq(i)
     if (chain_len > max):
         max = chain_len
-print (max)
+        st_num = start_number
+print (max ," AND " ,st_num )
