835b655fa52451a1f6e96e376c91b8c1fab030be
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-08-27 09:52:18 +0200

    Update eulerProject14.py

--- eulerProject14.py
+++ eulerProject14.py
@@ -1,15 +1,19 @@
 # product Collatz Sequence and return length of chain
 
 def collatz_seq(start_number):
-    seq = []
+    seq = [start_number]
     while(True):
+        if (seq[-1]==1) :
+            return len(seq)
         if (start_number%2 == 0):
             seq.append(start_number/2)
         else:
             seq.append((3*start_number)+1)
-        if (seq[-1]==1) :
-            return len(seq)
         start_number = seq[-1]
-        
-for i in range(1, 1000000):
+    
+max = 0    
+for i in range(1, 14):
     chain_len = collatz_seq(i)
+    if (chain_len > max):
+        max = chain_len
+print (max)
