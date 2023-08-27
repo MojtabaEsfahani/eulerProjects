b528aa06720e9d2899b1d2fad99740e53e003f39
Mojtaba Esfahani <107935120+MojtabaEsfahani@users.noreply.github.com>
2023-08-27 14:32:11 +0200

    Update eulerProject17.py

--- eulerProject17.py
+++ eulerProject17.py
@@ -1,11 +1,22 @@
 from num2words import num2words
 
-counter = 0
-for i in range(359, 1000):
-    word = num2words(i)
-    for j in word:
-        if(j=='-' or j==' '):
-            continue
-        else:
-            counter +=1
+# counter = 0
+# for i in range(359, 1001):
+#     word = num2words(i)
+#     for j in word:
+#         if(j=='-' or j==' '):
+#             continue
+#         else:
+#             counter +=1
             
+# print(counter)
+
+counter = 0
+word = num2words(1000)
+for j in word:
+    if(j=='-' or j==' '):
+        continue
+    else:
+        counter +=1
+            
+print(counter)
